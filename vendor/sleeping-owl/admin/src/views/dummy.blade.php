fd5d11da15716ffa9a3a16ab3fb05d4f516fe9e4
Omar Orozco <omar.orozco@agence.cl>
2017-01-23 21:09:44 -0400

    segundo

---
+++ vendor/sleeping-owl/admin/src/views/dummy.blade.php
@@ -0,0 +1,8 @@
+<div class="row">
+	<div class="col-lg-12">
+		<h1 class="page-header">Welcome to the administrative panel</h1>
+		<div class="alert alert-info">
+			Use the side menu to navigate through the different functionalities of the system.
+		</div>
+	</div>
+</div>
