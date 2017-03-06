295aa844ec3d795c5da1107a2a8833fd3c2f2ce6
omar <omar@debian>
2017-03-06 02:41:58 -0500

    icon direccion del footer

--- resources/views/main/portofolio.blade.php
+++ resources/views/main/portofolio.blade.php
@@ -159,7 +159,7 @@
 							<div class="contact-details">
 								<h4>{!! trans($metas['contact-title']) !!}</h4>
 								<ul class="contact">
-									<li><p><i class="icon-map-marker"></i> 
+									<li><p><i class="icon-map"></i> 
 									{!! trans($metas['contact-field-one']) !!}
 									</li>
 									<li><p><i class="icon-phone"></i> 
