0db388d29a9d602a1386ab45bdec40e61381911b
Jose <solorzano202009@gmail.com>
2017-03-29 14:25:44 -0400

    Deshabilitar responsive del header de la plantilla base de portofolios y customers (app.blade.php) para crear un standar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -49,7 +49,7 @@
 	</head>
 	<body>
 		<div class="body">
-			<header id="header" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyStartAt': 57, 'stickySetTop': '-57px', 'stickyChangeLogo': true}">
+			<header id="header" >
 				<div class="header-body">
 					<div class="header-container container">
 						<div class="header-row">
