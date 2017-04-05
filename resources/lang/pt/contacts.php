d1fe4c2ac3efac323322b5da0344229de00ef28f
Jose <solorzano202009@gmail.com>
2017-04-05 17:07:20 -0400

    Aplicación de traducción de las páginas de contactos.

---
+++ resources/lang/pt/contacts.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'title' => '<strong>Contact</strong> Us',
+	'label-name' => 'O seu nome *',
+	'label-mail' => 'O seu e-mail *',
+	'label-asunto' => 'Assunto',
+	'label-message' => 'A sua mensagem *',
+	'button-send' => 'Send Message',
+	'nom-bandera' => 'Sede Luanda-Angola',
+	'label-address' => '<strong>Address:</strong> Condomínio Mirantes do Talatona - Casa D20 – Talatona | Luanda – Angola',
+	'label-phone' => '<strong>Phone:</strong> (+244) 222 019 224',
+	'label-phone-address' => '<strong>Email:</strong> <a href="mailto:mail@example.com">atendimento@itemao.com</a>',
+	'label-text-h3' => 'Para trabalhar conosco, <strong>clique aquí</strong>',
+	'button-work-with-us' => 'Trabalhe connosco',
+];
---
+++ resources/lang/pt/work_with_us.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+	'text-presentation' => 'Para entrar em contato connosco, preencha o formulário abaixo ou ligue para o número:',
+	'text-title' => 'Dados Pessoais',
+	'label-name' => 'Nome: *',
+	'label-mail' => 'e-mail: *',
+	'label-endereo' => 'Endereço:',
+	'label-provincia' => 'Província:',
+	'label-comuna' => 'Comuna:',
+	'label-passport' => 'BI ou Passaporte:',
+	'label-sexo' => 'Sexo*:',
+	'label-sexo-default' => 'Seleccione ::',
+	'label-sexo-m' => 'Masculino',
+	'label-sexo-f' => 'Feminino',
+	'label-estado-civil' => 'Estado Civil*:',
+	'label-estado-civil-default' => 'Seleccione ::',
+	'label-estado-civil-solt' => 'Solteiro (a)',
+	'label-estado-civil-cas' => 'Casado (a)',
+	'label-estado-civil-div' => 'Divorciado (a)',
+	'label-estado-civil-viu' => 'Viuvo (a)',
+	'label-nacimiento' => 'Nascimento*:',
+	'label-nacionalidad' => 'Nacionalidade*:',
+	'label-naturalidad' => 'Naturalidade*:',
+	'label-telefono' => 'Telefone*:',
+	'label-anexo' => 'Anexar CV*:',
+	'buttom-send' => 'Enviar',
+];
