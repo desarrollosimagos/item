38ae4604fba53c80ca45a2c3c7293e25a5a5fc62
Desarrollos Imagos <desarrollosimago@gmail.com>
2017-04-07 09:22:30 -0700

    traducciones 7-4-2016

--- resources/lang/pt/contacts.php
+++ resources/lang/pt/contacts.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-	'title' => '<strong>Contact</strong> Us',
+	'title' => '<strong>Fale</strong> Connosco',
 	'label-name' => 'O seu nome *',
 	'label-mail' => 'O seu e-mail *',
 	'label-asunto' => 'Assunto',
 	'label-message' => 'A sua mensagem *',
-	'button-send' => 'Send Message',
-	'nom-bandera' => 'Sede Luanda-Angola',
-	'label-address' => '<strong>Address:</strong> Condomínio Mirantes do Talatona - Casa D20 – Talatona | Luanda – Angola',
-	'label-phone' => '<strong>Phone:</strong> (+244) 222 019 224',
+	'button-send' => 'Enviar',
+	'label-address1' => '<strong>Sede Luanda-Angola:</strong> Condomínio Mirantes do Talatona - Casa D20 – Talatona | Luanda – Angola',
+	'label-address2' => '<strong>Filial Malanje:</strong> Bairro Azul, Rua Comandante Dangereux',
+	'label-phone' => '<strong>Telf.:</strong> (+244) 222 019 224',
 	'label-phone-address' => '<strong>Email:</strong> <a href="mailto:mail@example.com">atendimento@itemao.com</a>',
 	'label-text-h3' => 'Para trabalhar conosco, <strong>clique aquí</strong>',
 	'button-work-with-us' => 'Trabalhe connosco',
